985e0842dc2cff84cf525c9bdecea2954d6cf580
SnowFox <kwebdev225@gmail.com>
2021-02-25 09:11:03 -0500

    Update comment

---
+++ src/views/admin/AdminView/Comments/index.tsx
@@ -0,0 +1,75 @@
+import React, { FC, useCallback, useState, useEffect } from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import {
+  Box,
+  Card,
+  CardHeader,
+  Divider,
+  List,
+  makeStyles
+} from '@material-ui/core';
+import axios from 'src/utils/axios';
+import useIsMountedRef from 'src/hooks/useIsMountedRef';
+import GenericMoreButton from 'src/components/GenericMoreButton';
+import { FlagWithUser } from 'src/types/flag';
+import FlagItem from './FlagItem';
+
+interface TeamTasksProps {
+  className?: string;
+}
+
+const useStyles = makeStyles(() => ({
+  root: {}
+}));
+
+const TeamTasks: FC<TeamTasksProps> = ({ className, ...rest }) => {
+  const classes = useStyles();
+  const isMountedRef = useIsMountedRef();
+  const [flags, setFlags] = useState<FlagWithUser[]>([]);
+
+  const getFlags = useCallback(async () => {
+    try {
+      const response = await axios.get<{ flags: FlagWithUser[] }>(
+        '/comments/flagsAll'
+      );
+
+      if (isMountedRef.current) {
+        setFlags(response.data.flags);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }, [isMountedRef]);
+
+  useEffect(() => {
+    getFlags();
+  }, [getFlags]);
+
+  return (
+    <Card className={clsx(classes.root, className)} {...rest}>
+      <CardHeader action={<GenericMoreButton />} title="Reports" />
+      <Divider />
+      <PerfectScrollbar>
+        <Box minWidth={400}>
+          <List>
+            {flags.map((flag, i) => (
+              <FlagItem
+                divider={i < flags.length - 1}
+                key={flag._id}
+                flag={flag}
+              />
+            ))}
+          </List>
+        </Box>
+      </PerfectScrollbar>
+    </Card>
+  );
+};
+
+TeamTasks.propTypes = {
+  className: PropTypes.string
+};
+
+export default TeamTasks;
